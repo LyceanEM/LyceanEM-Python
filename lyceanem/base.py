93af44efcb63ec474a1127b4624d079f2b09ab64
Timothy Pelham <t.g.pelham@bristol.ac.uk>
2022-04-25 14:20:41 +0100

    Created open3drotate in geometry functions to encapsulate the open3d rotate method, this should allow versions higher than 0.9.0 to be used where supported, and allow the full package to be used with more recent versions of python, as the 0.9.0 verison of open3d only supported python 3.6. I will need to update the examples to show how to use the encapsulated rotation function. I have included the first example of how to use aperture projection.

--- lyceanem/base.py
+++ lyceanem/base.py
@@ -81,29 +81,7 @@ scattering_point = np.dtype([
 
 scattering_t = from_dtype(scattering_point)  # Create a type that numba can recognize!
 
-class PNGScraper(object):
-    def __init__(self):
-        self.seen = set()
 
-    def __repr__(self):
-        return 'PNGScraper'
-
-    def __call__(self, block, block_vars, gallery_conf):
-        # Find all PNG files in the directory of this example.
-        path_current_example = os.path.dirname(block_vars['src_file'])
-        pngs = sorted(glob(os.path.join(path_current_example, '*.png')))
-
-        # Iterate through PNGs, copy them to the sphinx-gallery output directory
-        image_names = list()
-        image_path_iterator = block_vars['image_path_iterator']
-        for png in pngs:
-            if png not in self.seen:
-                self.seen |= set(png)
-                this_image_path = image_path_iterator.next()
-                image_names.append(this_image_path)
-                shutil.move(png, this_image_path)
-        # Use the `figure_rst` helper function to generate rST for image files
-        return figure_rst(image_names, gallery_conf['src_dir'])
 
 @njit
 def cart2pol(x, y):
