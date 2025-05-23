4c2845f42cb07b92bb8125907ce41a36b9ab18aa
tf17270 <tfearon38@gmail.com>
2025-05-23 13:12:59 +0100

    add debug prints to build.bat

--- conda/bld.bat
+++ conda/bld.bat
@@ -9,8 +9,13 @@ set "CMAKE_GENERATOR=Ninja"
 REM Determine Python version
 for /f %%v in ('%PYTHON% -c "import sys; print(f'python{sys.version_info.major}{sys.version_info.minor}')"') do set PYTHON_LIB=%%v.lib
 
-REM Check if pythonXXX.lib exists
-set "PYTHON_LIB=%PREFIX%\libs\python39.lib"
+REM Get Python version (e.g., python39)
+for /f %%v in ('%PYTHON% -c "import sys; print(f'python{sys.version_info.major}{sys.version_info.minor}')"') do set PYTHON_LIB_NAME=%%v
+
+REM Set full path to the expected .lib file
+set "PYTHON_LIB=%PREFIX%\libs\%PYTHON_LIB_NAME%.lib"
+
+REM Check if it exists
 if not exist "%PYTHON_LIB%" (
     echo ERROR: Required file "%PYTHON_LIB%" not found!
     echo This file is needed to link your C++/CUDA extension with Python.
@@ -19,10 +24,6 @@ if not exist "%PYTHON_LIB%" (
 else (
     echo Found Python library: %PYTHON_LIB%
 )
-if not exist "%LIBRARY_LIB%\%PYTHON_LIB%" (
-  echo ERROR: Required file "%LIBRARY_LIB%\%PYTHON_LIB%" not found!
-  echo This file is needed to link your C++/CUDA extension with Python.
-  exit /b 1
-)
+
 REM Build the package using pip + scikit-build
 %PYTHON% -m pip install . --no-deps --no-build-isolation -vv || exit /b 1
