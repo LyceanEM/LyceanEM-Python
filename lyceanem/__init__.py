a91360a2b456fca582b6110f6d94d96759d6d781
LyceanEM <60020395+LyceanEM@users.noreply.github.com>
2025-05-27 14:01:44 +0100

    added import statement to populate em to lyceanem workspace.

--- lyceanem/__init__.py
+++ lyceanem/__init__.py
@@ -13,7 +13,9 @@ except metadata.PackageNotFoundError:
     # package is not installed
     pass
 
+from . import em
+
 __copyright__ = """\
-© Timothy Pelham 2016-2024
+© Timothy Pelham 2016-2025
 """
 __license__ = "GNU Affero General Public License v3"
